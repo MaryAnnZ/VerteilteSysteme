11d2d66dd8371f0acee38a636e18d4d215bf74b6
Carina <carina.pratsch@gmail.com>
2016-01-05 15:25:41 +0100

    Revert "implemented message integrity - not tested"
    This reverts commit cf9e47ae94fcf899620817745a8fc175bc9be876.

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -33,11 +33,6 @@ import cli.Shell;
 import util.Config;
 import util.Keys;
 
-import java.security.Key;
-import java.security.MessageDigest;
-
-import javax.crypto.Mac;
-
 public class Client implements IClientCli, Runnable {
 
 	private String componentName;
@@ -193,35 +188,9 @@ public class Client implements IClientCli, Runnable {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(privateSocket.getInputStream()));
 		PrintWriter writer = new PrintWriter(privateSocket.getOutputStream(), true);
 		
-		// create and encode hmac for message
-		byte [] hash = Base64.encode(createHMAC(message));
-		writer.println(hash + "_!msg_" + message);
-		
-		// get response and vertify message wasnt changed	
-		parts = reader.readLine().split("_");
-		
-		byte [] receivedHash = Base64.decode(parts[0].getBytes());
-		String receivedCommand = parts[1];
-		String receivedMessage = parts[2];
-		
-		// create new hmac to vertify
-		byte[] computedHash = createHMAC(receivedMessage);
-		
-		boolean validHash = MessageDigest.isEqual(computedHash, receivedHash);
-		
-		
-		if(validHash && receivedCommand.equals("!ack")) {
-			return username + " replied with " + receivedCommand;
-		}
-		else if(validHash && receivedCommand.equals("!tampered")) {
-			return username + " recieved tampered message!";
-		}
-		else if(!validHash && receivedCommand.equals("!ack")) {
-			return "response from " + username + "was tampered!";
-		}
-		else {
-			return "entire conversation with " + username + " was tampered!";
-		}
+		writer.println(message);
+				
+		return username + " replied with " + reader.readLine();
 	}
 
 	@Override
@@ -243,7 +212,7 @@ public class Client implements IClientCli, Runnable {
 			String[] parts = privateAddress.split(":");
 			
 			serverSocket = new ServerSocket(Integer.parseInt(parts[1]));
-			threadPool.execute(new PrivateMessageListener(this, serverSocket, Keys.readSecretKey(new File(config.getString("hmac.key")))));
+			threadPool.execute(new PrivateMessageListener(this, serverSocket));
 		}
 			
 		return response;
@@ -306,27 +275,6 @@ public class Client implements IClientCli, Runnable {
 		return null;
 	}
 	
-	private byte[] createHMAC(String message) throws IOException {
-		
-		// read the shared secret key
-		Key secretKey = Keys.readSecretKey(new File(config.getString("hmac.key")));
-		
-		// create HMAC
-		Mac hMac;
-		byte[] hash = null;
-		
-		try {
-			hMac = Mac.getInstance("HmacSHA256");
-			hMac.init(secretKey);
-			hMac.update(message.getBytes());
-			hash = hMac.doFinal();
-			
-		} catch (NoSuchAlgorithmException e) { e.printStackTrace();
-		} catch (InvalidKeyException e) { e.printStackTrace(); }
-		
-		return hash;
-	}
-	
 	/**
 	 * @param args
 	 *            the first argument is the name of the {@link Client} component
@@ -342,7 +290,6 @@ public class Client implements IClientCli, Runnable {
 	// implement them for the first submission. ---
 
 	@Override
-	@Command
 	public String authenticate(String username) throws IOException {
 		//generate challenge
 
--- src/main/java/client/PrivateMessageListener.java
+++ src/main/java/client/PrivateMessageListener.java
@@ -6,26 +6,19 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import javax.crypto.Mac;
 
-import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
-import com.sun.org.apache.xml.internal.security.utils.Base64;
+import chatserver.handler.ClientHandler;
 
 public class PrivateMessageListener extends Thread {
 	
 	private Client client;
 	private ServerSocket serverSocket;
-	private final Key secretKey;
 	
-	public PrivateMessageListener(Client client, ServerSocket serverSocket, Key secretKey) {
+	public PrivateMessageListener(Client client, ServerSocket serverSocket) {
 		this.client = client;
 		this.serverSocket = serverSocket;
-		this.secretKey = secretKey;
 	}
+	
 	
 	public void run() {
 		
@@ -43,46 +36,19 @@ public class PrivateMessageListener extends Thread {
 				PrintWriter writer = new PrintWriter(socket.getOutputStream(),
 						true);
 
-				// read client message				
-				String[] parts = reader.readLine().split("_");
-				
-				byte [] receivedHash = Base64.decode(parts[0].getBytes());
-				String command = parts[1];
-				String message = parts[2];
-				
-				if(command.equals("!msg")) {
-					
-					// create new hmac to vertify
-					Mac hMac;
-					byte[] computedHash = null;
-					
-					try {
-						hMac = Mac.getInstance("HmacSHA256");
-						hMac.init(secretKey);
-						hMac.update(message.getBytes());
-						computedHash = hMac.doFinal();
-						
-					} catch (NoSuchAlgorithmException e) { e.printStackTrace(); 
-					} catch (InvalidKeyException e) { e.printStackTrace(); }
-					
-					// print response
-					if(MessageDigest.isEqual(computedHash, receivedHash)) {
-						writer.println(Base64.encode(computedHash) + "_!ack_" + message);
-					} else {
-						writer.println(Base64.encode(computedHash) + "_!tampered_" + message);
-					}
+				// read client message
+				String message = reader.readLine();
+					// print request
+					writer.println("!ack");
 					
 					client.writePrivateMessage(message);
 					break;
-				}
 
 			} catch (IOException e) {
 				/*System.err
 						.println("Error occurred while waiting for/communicating with client: "
 								+ e.getMessage());*/
 				break;
-			} catch (Base64DecodingException e1) {
-				e1.printStackTrace();
 			} finally {
 				if (socket != null && !socket.isClosed())
 					try {
